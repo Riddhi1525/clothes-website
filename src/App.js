b08322e3e73ce07c8eca8d835a017934675b92d2
Riddhi1525 <riddhikothari2001@gmail.com>
2022-04-22 09:38:00 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import {title} from './App.css';
+
+import { Products } from './Components/Products/Product';
+import { productData, productDataTwo } from './Components/Products/Data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+       <title heading="ECOMMERCE WEBSITE"/>
+        <Products heading="ECOMMERCE WEBSITE" data={productData} />
+        
+        <Products heading="FOR WOMEN" data={productDataTwo} />
+        
+      </BrowserRouter>
+
     </div>
   );
 }
-
 export default App;
---
+++ src/Components/Products/Data.js
@@ -0,0 +1,67 @@
+import product1 from '../../Images/product1.jpeg';
+import product2 from '../../Images/product1.jpeg';
+import product3 from '../../Images/product1.jpeg';
+import W1 from '../../Images/product2.jpg';
+import W2 from '../../Images/product2.jpg';
+import W3 from '../../Images/product2.jpg';
+
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Solid Men High Neck Dark Blue T-S',
+        name: 'Solid Men High Neck Dark Blue T-S',
+        desc:
+            'Solid Men High Neck Dark Blue T-S',
+        price: 'RS 650',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Solid Men High Neck Dark Blue T-S',
+        name: 'Solid Men High Neck Dark Blue T-S',
+        desc:
+            'Solid Men High Neck Dark Blue T-S',
+        price: 'RS 650',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Solid Men High Neck Dark Blue T-S',
+        name: 'Solid Men High Neck Dark Blue T-S',
+        desc:
+            'Solid Men High Neck Dark Blue T-S',
+        price: 'RS 650',
+        button: 'Add to Cart'
+    }
+];
+
+export const productDataTwo = [
+    {
+        img: W2,
+        alt: 'Miss Chase Casual Regular SleeVE',
+        name: 'Miss Chase Casual Regular SleeVE',
+        desc:
+            'Miss Chase Casual Regular SleeVE',
+        price: 'RS 650',
+        button: 'Add to Cart'
+    },
+    {
+        img: W3,
+        alt: 'Miss Chase Casual Regular SleeVE',
+        name: 'Miss Chase Casual Regular SleeVE',
+        desc:
+            'Miss Chase Casual Regular SleeVE',
+        price: 'RS 650',
+        button: 'Add to Cart'
+    },
+    {
+        img: W1,
+        alt: 'Miss Chase Casual Regular SleeVE',
+        name: 'Miss Chase Casual Regular SleeVE',
+        desc:
+            'Miss Chase Casual Regular SleeVE',
+        price: 'RS 650',
+        button: 'Add to Cart'
+    }
+];
